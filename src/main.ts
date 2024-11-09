513d70afe4e4540befaf97c2bd4c66ca51113e30
Olanike <alexisdame2017@gmail.com>
2024-11-09 07:57:55 +0100

    update server file

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors({
-    origin: ['*'],
+    origin: '*',
     methods: 'GET,POST',
     allowedHeaders: 'Content-Type,Authorization',
   });
