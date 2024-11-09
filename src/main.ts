565785505f995605e567b03b6bfb7e29314e1d77
Olanike <alexisdame2017@gmail.com>
2024-11-09 07:49:26 +0100

    update server file

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,12 @@ import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.use(
-    '/graphql',
-    cors<cors.CorsRequest>({
-      origin: ['https://webs-integrate.vercel.app', 'http://localhost:3000'],
-      methods: 'GET,POST',
-      allowedHeaders:
-        'Content-Type,Authorization,x-apollo-operation-name,apollo-require-preflight',
-    }),
-  );
 
   app.enableCors({
-    origin: ['https://webs-integrate.vercel.app', 'http://localhost:3000'],
+    origin: ['*'],
     methods: 'GET,POST',
     allowedHeaders: 'Content-Type,Authorization',
   });
